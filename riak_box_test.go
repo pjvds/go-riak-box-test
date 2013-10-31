92e01d6f91c55e780da4b3babd459ab8ee6369d9
Pieter Joost van de Sande <pj@born2code.net>
2013-10-31 14:24:01 +0100

    Use MJDSYS_RIAK_HOST

--- riak_box_test.go
+++ riak_box_test.go
@@ -15,13 +15,13 @@ func TestBooks(t *testing.T) {
 
 var _ = Describe("Riak", func() {
 	Context("When using the riak box at wercker", func() {
-		pbconnect := os.Getenv("MJDSYS_RIAK_PBCONNECT")
+		host := os.Getenv("MJDSYS_RIAK_HOST")
 
-		It("should export PBCONNECT", func() {
-			Expect(pbconnect).ToNot(BeEmpty())
+		It("should export MJDSYS_RIAK_HOST", func() {
+			Expect(host).ToNot(BeEmpty())
 		})
-		It("should accept connections via pbconnect", func() {
-			client := riak.New(pbconnect)
+		It("should accept connections", func() {
+			client := riak.New(host)
 			err := client.Connect()
 			Expect(err).To(BeNil())
 
