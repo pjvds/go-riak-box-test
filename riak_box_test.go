95bce714d06bcde67d7f58902c263b3035e7f6af
Pieter Joost van de Sande <pj@born2code.net>
2013-10-31 13:58:26 +0100

    Test pbconnect and httpconnect

--- riak_box_test.go
+++ riak_box_test.go
@@ -15,11 +15,24 @@ func TestBooks(t *testing.T) {
 
 var _ = Describe("Riak", func() {
 	Context("When using the riak box at wercker", func() {
+		pbconnect := os.Getenv("MJDSYS_RIAK_PBCONNECT")
+		httpconnect := os.Getenv("MJDSYS_RIAK_HTTPCONNECT")
+
 		It("should export PBCONNECT", func() {
-			Expect(os.Getenv("MJDSYS_RIAK_PBCONNECT")).ToNot(BeEmpty())
+			Expect(pbconnect).ToNot(BeEmpty())
 		})
-		It("should accept connections", func() {
-			client := riak.New(os.Getenv("MJDSYS_RIAK_PBCONNECT"))
+		It("should export HTTPCONNECT", func() {
+			Expect(httpconnect).ToNot(BeEmpty())
+		})
+		It("should accept connections via pbconnect", func() {
+			client := riak.New(bpconnect)
+			err := client.Connect()
+			Expect(err).To(BeNil())
+
+			client.Close()
+		})
+		It("should accept connections via httpconnect", func() {
+			client := riak.New(httpconnect)
 			err := client.Connect()
 			Expect(err).To(BeNil())
 
